d1ed273fd5afa5ad103083a01d6fc410f5e4dc75
nekrosb <makarov.nikita.mn@gmail.com>
2026-02-18 14:18:56 +0100

    feat: create todos database

---
+++ src/db.ts
@@ -0,0 +1,15 @@
+import { Database } from "bun:sqlite";
+
+export const db = new Database("./data/mydb.sqlite", {create: true});
+
+  
+
+db.run(`
+    create table if not exists todos (
+    id integer primary key autoincrement,
+    title text not null,
+    content text,
+    due_date text,
+    done boolean not null
+    );
+    `)
