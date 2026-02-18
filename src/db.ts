d881ad8675c898f5d47ba9fa4b6e331fb1317c79
nekrosb <makarov.nikita.mn@gmail.com>
2026-02-18 14:39:43 +0100

    refactor(db.ts): move table creation to initDatabase function

--- src/db.ts
+++ src/db.ts
@@ -2,12 +2,15 @@ import { Database } from "bun:sqlite";
 
 export const db = new Database("./data/mydb.sqlite", { create: true });
 
-db.run(`
+export function initializeDatabase() {
+    db.run(`
     create table if not exists todos (
     id integer primary key autoincrement,
     title text not null,
     content text,
     due_date text,
+    // date is yyyy-mm-dd
     done boolean not null
     );
     `);
+}
