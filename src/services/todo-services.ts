fa29108217de29d8cfffcde17a774c32b0019176
nekrosb <makarov.nikita.mn@gmail.com>
2026-02-19 14:20:34 +0100

    feat: create patch function

--- src/services/todo-services.ts
+++ src/services/todo-services.ts
@@ -1,5 +1,5 @@
 import { db } from "../db.ts";
-import { boolean, object, parse, string } from "valibot";
+import { boolean, object, optional, parse, string } from "valibot";
 
 export const getTodos = async () => {
   const todos = db.prepare("SELECT * FROM todos").all();
@@ -36,6 +36,71 @@ export const createTodo = async (req: Request) => {
     return new Response(JSON.stringify({ error: "Failed to create todo" }), {
       headers: { "Content-Type": "application/json" },
       status: 400,
+    });
+  }
+};
+const reqUpdateValidator = object({
+  title: optional(string()),
+  content: optional(string()),
+  due_date: optional(string()),
+  done: optional(boolean()),
+});
+
+export const updateTodo = async (req: Request) => {
+  const id = Number(req.url.split("/").pop());
+
+  try {
+    const body = await req.json();
+    const validated = parse(reqUpdateValidator, body);
+    const f: string[] = [];
+    const d: any[] = [];
+
+    if (validated.title !== undefined && validated.title !== null) {
+      f.push("title");
+      d.push(validated.title);
+    }
+    if (validated.content !== undefined && validated.content !== null) {
+      f.push("content");
+      d.push(validated.content);
+    }
+    if (
+      validated.due_date !== undefined && validated.due_date !== null
+    ) {
+      f.push("due_date");
+      d.push(validated.due_date);
+    }
+    if (validated.done !== undefined && validated.done !== null) {
+      f.push("done");
+      d.push(validated.done);
+    }
+
+    if (f.length === 0) {
+      return new Response(JSON.stringify({ error: "No fields to update" }), {
+        headers: { "Content-Type": "application/json" },
+        status: 400,
+      });
+    }
+
+    const query = `UPDATE todos SET ${
+      f.map((field) => `${field} = ?`).join(", ")
+    } WHERE id = ?`;
+    d.push(id);
+    db.run(query, d);
+
+    return new Response(
+      JSON.stringify({
+        message: "Todo updated successfully",
+      }),
+      {
+        headers: { "Content-Type": "application/json" },
+        status: 200,
+      },
+    );
+  } catch (err) {
+    console.log(err);
+    return new Response(JSON.stringify({ error: "Failed to update todo" }), {
+      headers: { "Content-Type": "application/json" },
+      status: 500,
     });
   }
 };
